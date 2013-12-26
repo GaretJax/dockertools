0a38e6cff5cd52f4e231fd795f1929d0f712a3b4
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-12-26 10:31:00 +0100

    Initial commit

---
+++ dockertools/__init__.py
@@ -0,0 +1,9 @@
+"""
+docker-tools
+
+MIT License. See LICENSE for more details.
+Copyright (c) 2013, Jonathan Stoppani
+"""
+
+__version__ = '0.1.0'
+__url__ = 'https://github.com/GaretJax/dockertools'
