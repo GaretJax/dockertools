9062b4a245edb18daf7c859913cd503fcd71be92
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-08-22 13:03:17 +0200

    Update requirements (and remove monkey patching).

--- dockertools/__init__.py
+++ dockertools/__init__.py
@@ -7,19 +7,3 @@ Copyright (c) 2013, Jonathan Stoppani
 
 __version__ = '0.1.0'
 __url__ = 'https://github.com/GaretJax/dockertools'
-
-
-# TODO: Monkey path requests 1.2.3 to support SNI. The lateste version supports
-# this out of the box, but docker-py currently requires requests==1.2.3
-
-import requests
-
-if hasattr(requests, 'pyopenssl'):
-    def fileno(self):
-        return self.socket.fileno()
-
-    def close(self):
-        return self.connection.shutdown()
-
-    requests.pyopenssl.WrappedSocket.close = close
-    requests.pyopenssl.WrappedSocket.fileno = fileno
