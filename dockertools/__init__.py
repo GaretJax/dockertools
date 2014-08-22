3d78ce697e395429b0a2b47d16033a74cabdf84d
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-08-22 13:03:24 +0200

    Version bump.

--- dockertools/__init__.py
+++ dockertools/__init__.py
@@ -5,5 +5,5 @@ MIT License. See LICENSE for more details.
 Copyright (c) 2013, Jonathan Stoppani
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __url__ = 'https://github.com/GaretJax/dockertools'
